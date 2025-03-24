6f63cd7c5a04230ea33e1a58e0f8aeccc583b6ac
ShahzanTariq <shahzantariq@gmail.com>
2025-03-24 03:29:03 -0400

    fix: precheck hash now runs when main.py is ran

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from GUI import GUI
+from transformer import precheck_hash_dupe
 
 if __name__ == "__main__":
+    precheck_hash_dupe()
     app = QApplication(sys.argv)
     window = GUI()
     window.show()
